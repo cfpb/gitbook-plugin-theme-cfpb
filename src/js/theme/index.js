2f3a3eae9b2581e3c429d11b2d7761a10cca866e
Ans <anselmbradford@users.noreply.github.com>
2017-12-28 20:22:34 -0500

    Add files via upload

---
+++ src/js/theme/index.js
@@ -0,0 +1,33 @@
+var dropdown =   require('./dropdown');
+var keyboard =   require('./keyboard');
+var sidebar =    require('./sidebar');
+var toolbar =    require('./toolbar');
+
+var gitbook = window.gitbook;
+
+function init() {
+    // Init sidebar
+    sidebar.init();
+
+    // Init keyboard
+    keyboard.init();
+
+    // Bind dropdown
+    dropdown.init();
+
+    // Add action to toggle sidebar
+    toolbar.createButton({
+        index: 0,
+        icon: 'fa fa-align-justify',
+        onClick: function(e) {
+            e.preventDefault();
+            sidebar.toggle();
+        }
+    });
+}
+
+gitbook.events.on('start', init);
+
+gitbook.keyboard = keyboard;
+gitbook.sidebar = sidebar;
+gitbook.toolbar = toolbar;
